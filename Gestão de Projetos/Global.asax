6cacba974d5e7aebc19e79c44783b7efb33efab9
Samuel <samhaw@ig.com.br>
2016-06-19 01:04:51 -0300

    Inicio

---
+++ Gestão de Projetos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gestão_de_Projetos.MvcApplication" Language="C#" %>
---
+++ Gestão de Projetos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gestão_de_Projetos.Startup))]
+namespace Gestão_de_Projetos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
